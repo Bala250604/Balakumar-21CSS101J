e936190b3698b1b63d0550fd5fb5f687f246309a
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-08 11:21:11 +0530

    Create 65. ASCII value.c

---
+++ 65. ASCII value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main ()
+{
+
+  char ch;
+  
+  printf("Enter a Character to Find ASCII Value :");
+  scanf ("%c", &ch);
+
+  printf ("ASCII value of character %c: %i\n", ch, ch);
+
+  return 0;
+}
