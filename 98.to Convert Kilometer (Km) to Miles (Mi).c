16ee4f146d44bf1a73b137396d737285e4b3118b
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-09 11:55:48 +0530

    Create 98.to Convert Kilometer (Km) to Miles (Mi).c

---
+++ 98.to Convert Kilometer (Km) to Miles (Mi).c
@@ -0,0 +1,26 @@
+#include 
+//#include 
+
+void main() {
+
+  float km;
+  float miles;
+  
+
+//    clrscr();
+
+	printf(" Program to convert Kilometer to Miles :  ");
+
+	printf("\n\n Enter Length in Kilometer  : ");
+	scanf("%f", &km);
+    
+    miles = (0.621371*km);
+    
+    printf("\n\n %f Kilometer in Millimeter = %f  \n",km,miles);
+
+
+
+//	getch();
+
+
+}
