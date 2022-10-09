f2770bd213df777cfc135bcd74cae3fd27b29cc2
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-09 10:41:01 +0530

    Create 87.Fahrenheit to celcius.c

---
+++ 87.Fahrenheit to celcius.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float fahren, celsius;
+    printf("Enter temperature in Fahrenheit=");
+    scanf("%f", &fahren);
+    celsius = (fahren-32)*5.0 / 9.0;
+    printf("%.2f Fahrenheit is equal %.2f in       Celsius",fahren,celsius);
+    return 0;
+}
