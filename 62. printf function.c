a270c2edf817752d48449460c6ac46154c890701
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-12 17:15:23 +0530

    Update 62. printf function.c

--- 62. printf function.c
+++ 62. printf function.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-main()
+int main()
 {
-printf("You are learning printf() function");
+printf("You are learning printf() function\n");
 printf("You are learning printf() function");
 }
