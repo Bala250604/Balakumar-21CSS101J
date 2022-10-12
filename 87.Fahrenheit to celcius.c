530fa2e4b1025342c7ae8713f5b0a3f7c80e3825
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-12 17:08:05 +0530

    Update 87.Fahrenheit to celcius.c

--- 87.Fahrenheit to celcius.c
+++ 87.Fahrenheit to celcius.c
@@ -5,6 +5,6 @@ int main()
     printf("Enter temperature in Fahrenheit=");
     scanf("%f", &fahren);
     celsius = (fahren-32)*5.0 / 9.0;
-    printf("%.2f Fahrenheit is equal %.2f in       Celsius",fahren,celsius);
+    printf("%.2f Fahrenheit is equal %.2f in Celsius",fahren,celsius);
     return 0;
 }
