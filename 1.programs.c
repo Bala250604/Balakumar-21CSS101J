419778d088732ef4f14833bb561d695d4ace2a70
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-12 17:25:28 +0530

    Update 1.programs.c

--- 1.programs.c
+++ 1.programs.c
@@ -5,10 +5,7 @@ int main() {
   printf("RA2211042010053");
   printf("Enter two numbers to add\n");
   scanf("%d%d", &num1, &num2);
-
   addition = num1 + num2;
-  
   printf("Sum of the numbers = %d\n", addition);
-
   return 0;
 }
