89e0520b3d38aafc657fbff18b0ba5a2af08be9b
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-13 08:36:50 +0530

    Create secretNumber.c

---
+++ secretNumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+    int secretNumber = 5;
+    int guess;
+    while(guess != secretnumber){
+        printf("Enter a number: ")
+        scanf("%d", &guess);
+    }
+    printf("you win");
+    return 0;
+ }
+        
