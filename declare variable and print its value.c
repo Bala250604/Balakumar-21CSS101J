1ad0f1709a99d12f56c04b7054d3bd5a32a513ef
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:39:51 +0530

    Update and rename 31. declare variable and print its value to  declare variable and print its value.c

---
+++ declare variable and print its value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main ()
+{
+  int a = 9, b = 10; 
+  float c = 5.5, d = 15.5; 
+  printf("RA2211042010053");
+  printf ("Values of integer variables are: \n");
+  printf ("%d\n%d \n", a, b); 
+  printf ("Values of floating variables are: \n"); 
+  printf ("%f \n%f", c, d);
+}
