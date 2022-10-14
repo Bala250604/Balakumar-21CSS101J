a03dde99a0ea658dff116d326daa9c85e12d38d4
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 17:21:16 +0530

    Update and rename 65. ASCII value.c to  ASCII value.c

--- ASCII value.c
+++ ASCII value.c
@@ -1,8 +1,8 @@
-
 #include<stdio.h>
 int main ()
 {
  char ch;
+ printf("RA2211042010053");
  printf("Enter a Character to Find ASCII Value :");
  scanf ("%c", &ch);
  printf ("ASCII value of character %c: %i\n", ch, ch);
