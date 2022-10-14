030f83d2e38b906949750cdb648c763717e68b13
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:48:29 +0530

    Update and rename 42. find area and perimeter of circle to  find area and perimeter of circle.c

--- find area and perimeter of circle.c
+++ find area and perimeter of circle.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-
-#define PI 3.14f /* Define the value of pie */
+#define PI 3.14f
 
 void main() {
-  /* Variable Declaration. */
   float radius, perimeter, area;
-
-  /* Taking input of the radious of the circle from the user */
+  printf("RA2211042010053");
   printf("Enter radius of the Circle:\n");
   scanf("%f", & radius);
-
-  /* Calculating perimeter of the circle */
   perimeter = 2 * PI * radius;
   printf("Perimeter of the circle: %0.4f\n", perimeter);
-
-  /* Calculating area of the circle */
   area = PI * radius * radius;
   printf("Area of circle: %0.4f\n", area);
 }
