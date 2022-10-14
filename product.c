d3b37da5b5235077573f4ccb1a0537a159d42267
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:33:01 +0530

    Update and rename 2.product to product.c

---
+++ product.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+     double a, b, product;
+     printf("RA2211042010053");
+     printf("Enter two numbers: ");
+     scanf("%lf %lf", &a, &b);  
+     product = a * b;
+     printf("Product = %.2lf", product);
+     return 0;
+}
