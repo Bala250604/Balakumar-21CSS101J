1e53a0313e0dfb41e632180b42bfa8f40fa5eba5
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:22:46 +0530

    Update and rename 16. sum of natural numbers to  sum of natural numbers.c

--- sum of natural numbers.c
+++ sum of natural numbers.c
@@ -2,13 +2,10 @@
 
 int main() {
     int n, i, sum = 0;
-
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-
     for (i = 1; i <= n; ++i) {
         sum += i;
     }
-
     printf("Sum = %d", sum);
     return 0;
