de0c5d03735ceb2551b24a0b22738eda0693b4c1
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:28:08 +0530

    Update and rename 26. length of string to  length of string.c

--- length of string.c
+++ length of string.c
@@ -2,9 +2,8 @@
 int main() {
     char s[] = "Programming is fun";
     int i;
-
     for (i = 0; s[i] != '\0'; ++i);
-    
+    printf("RA2211042010053");
     printf("Length of the string: %d", i);
     return 0;
 }
