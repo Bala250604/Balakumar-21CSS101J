4658bbc3022f2a9c3a040830bd8b085f64988e71
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 17:01:18 +0530

    Update and rename 57. biggest among three numbers to  biggest among three numbers.c

--- biggest among three numbers.c
+++ biggest among three numbers.c
@@ -5,18 +5,14 @@ void main()
 {
     int a,b,c;
     clrscr();
- 
+    printf("RA2211042010053");
     printf(" Enter Three Values : ");
     scanf("%d %d %d",&a,&b,&c);
- 
     if((a>b)&&(a>c))
         printf("\n %d is a Greatest Number",a);
- 
     if((b>c)&&(b>a))
         printf("\n %d is a Greatest Number",b);
- 
     if((c>a)&&(c>b))
         printf("\n %d is Greatest Number",c);
- 
     getch();
 }
