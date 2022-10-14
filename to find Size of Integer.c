9ce171900c16728856ec2e566fd8924398914f23
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 17:07:36 +0530

    Update and rename 60. to find Size of Integer to to find Size of Integer.c

--- to find Size of Integer.c
+++ to find Size of Integer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
+    printf("RA2211042010053");
     printf("\n size of short: %d", sizeof(short));
     printf("\n size of int: %d", sizeof(int));
     printf("\n size of long int: %d", sizeof(long));
