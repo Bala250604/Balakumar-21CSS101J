fc64688c23ffb17c592edfc3e3c8f37a91fc08eb
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:49:48 +0530

    Update and rename 88. person height from inches to centimeter.c to  person height from inches to centimeter.c

--- person height from inches to centimeter.c
+++ person height from inches to centimeter.c
@@ -3,9 +3,9 @@
 int main()
 {
    float inchesHeight, centiHeight;
+   printf("RA2211042010053");
    printf("Enter Person Height in inches =");
    scanf("%f", &inchesHeight);
-   
    centiHeight = inchesHeight * 2.54;
    printf("Height of Person in Centimeters =%.2f", centiHeight);
    return 0;
