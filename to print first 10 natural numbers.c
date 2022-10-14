941986424cfcb45eb36a10153d653f48e5c7159b
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-14 16:51:42 +0530

    Update and rename 45. to print first 10 natural numbers to  to print first 10 natural numbers.c

---
+++ to print first 10 natural numbers.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+
+void main() {
+  int i; 
+  printf("RA2211042010053");
+  printf("The first 10 natural numbers are:\n ");
+  for (i = 1; i <= 10; i++)
+  {
+    printf("%d \t", i); 
+  }
+}
