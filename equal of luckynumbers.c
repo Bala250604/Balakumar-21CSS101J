755ad47cdbd3cbbcb996a4e24b6828cc63c1a7a5
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-10-26 08:59:00 +0530

    Create equal of luckynumbers.c

---
+++ equal of luckynumbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int Luckynumbers[] = {4,8,15,48,32,60};
+    int i;
+    for(i=0;i<=6;i++){
+        printf("%d\n",i);
+    }
+    // RA2211042010053
+    return 0;
+}
