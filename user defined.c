c85199279c800545cba7ef68e6be00da2db567c7
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-12 20:33:40 +0530

    Create user defined.c

---
+++ user defined.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    enum day {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
+    enum day d = thursday;
+    printf("The day number stored in d is %d\n", d);
+    //RA2211042010053
+    return 0;
+}
