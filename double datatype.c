3576825ab0d684739382e68b084ca8a32d59f8bb
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-12 20:26:13 +0530

    Create double datatype.c

---
+++ double datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    double num1 = 15.479, num2 = 25.7, result;
+    printf("Given double values are num1 = %lf, num2 = %lf\n", num1,num2);
+    result = num2 / num1;
+    printf("Result of diving = %lf\n",result);
+    printf("Result of diving in exponential format = %e\n",result);
+    //RA2211042010053
+    return 0;
+}
