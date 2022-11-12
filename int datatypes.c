97b17bd6b4dac8ef3ec4c51967db77155794e6b4
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-12 12:25:50 +0530

    Create int datatypes.c

---
+++ int datatypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int binaryThree = 0b11;
+    printf("binaryThree value = %d\n", binaryThree);
+    int octalEight = 010;
+    printf("octalEight value = %d\n", octalEight);
+    int hexTen = 0XA;
+    printf("hexTen value = %d\n", hexTen);
+    int asciiValueOfOne = '1';
+    printf("asciiValueOfOne value = %d\n", asciiValueOfOne);
+    int asciiValueOfA = 'A';
+    printf("asciiValueOfA value = %d\n", asciiValueOfA);
+    //RA2211042010053
+    return 0;
+}
