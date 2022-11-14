e9c0468d12d554807e55e46b65fbd2e4bc7f9dbd
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-14 08:49:08 +0530

    Create int,float,char.c

---
+++ int,float,char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int n = 10;
+    float f = 20.5499;
+    char ch = 'A';
+    printf("Interger value = %d\nFloating point value = %f\nCharactervalue = %c\n",n,f,ch);
+//RA2211042010053
+    return 0;
+}
