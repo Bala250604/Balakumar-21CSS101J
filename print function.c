18db9f277d35c07e2a84446bf7ed905517f638b5
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-14 09:16:43 +0530

    Create print function.c

---
+++ print function.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Result1 = %f\n", (2 + 2.5));
+    printf("Result2 = %f\n", (65 + 10));
+    printf("Result3 = %f\n", (5.9 + 4.57));
+//RA2211042010053
+    return 0;
+}
