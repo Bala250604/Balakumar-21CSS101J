193addfeb5bf64d753c7f1095dfc4000b7a993ec
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-19 11:36:24 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int num = -23, result;
+    result = num;
+    printf("Bitwise one's complement result = %d\n", result);
+    //RA2211042010053
+    return 0;
+}
