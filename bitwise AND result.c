e7c7ec2330d7320cbaa52928eae2bff0c8b64309
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-19 11:45:41 +0530

    Create bitwise AND result.c

---
+++ bitwise AND result.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int num1 = 10, num2 = 3, result;
+    result = num1 & num2;
+    printf("Bitwise AND result = %d\n", result);
+    //RA2211042010053
+    return 0;
+}
