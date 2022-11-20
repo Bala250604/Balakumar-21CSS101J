bbffa1695313892b7d02267d03c4d7393541b670
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 11:50:31 +0530

    Create character oriented.c

---
+++ character oriented.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character : ");
+    ch = getchar();
+    printf("The entered character is : ");
+    putchar(ch);
+    //RA2211042010053
+    return 0;
+}
