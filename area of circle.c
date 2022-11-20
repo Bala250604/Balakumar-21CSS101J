340dee4305fd4a6e63fe00bc81d4dea09594afb1
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 21:56:55 +0530

    Create area of circle.c

---
+++ area of circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.14
+int main() {
+    float radius, area;
+    printf("Enter the radius : ");
+    scanf("%f",&radius);
+    area = PI*radius*radius;
+    printf("Area of circle = %f\n", area);
+    //RA2211042010053
+    return 0;
+}
