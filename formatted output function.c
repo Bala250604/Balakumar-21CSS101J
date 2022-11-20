2ced351ee734e219bc8d7084977f7c02a600caab
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 16:42:57 +0530

    Create formatted output function.c

---
+++ formatted output function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("%5d\n", 99);
+    printf("%06d\n", 99);
+    printf("%+8d\n", 9126);
+    printf("%+08d\n",9126);
+    //RA2211042010053
+    return 0;
+}
