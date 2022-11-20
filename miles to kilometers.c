21177ab5a2fdbeed4b132a080092e54ea666ed13
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 17:16:42 +0530

    Create miles to kilometers.c

---
+++ miles to kilometers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define MILES_TO_KMS 1.609
+int main() {
+    float miles, kilometers;
+    printf("Enter the distance in miles : ");
+    scanf("%f",&miles);
+    kilometers = miles*1.609;
+    printf("Distance in kilometers = %7.2f\n", Kilometers);
+    //RA2211042010053
+    return 0;
+}
