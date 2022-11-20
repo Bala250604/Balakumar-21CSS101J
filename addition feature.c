82c618c2a0383a02cc27f2c8e0294e6cd185e785
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 15:51:27 +0530

    Create addition feature.c

---
+++ addition feature.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int year,month,date;
+    printf("Enter DOB in yyy.mm.dd format : ");
+    scanf("%d %d %d",&year,&month,&date);
+    printf("DOB is : %d %d %d\n",year,month,date);
+    //RA2211042010053
+    return 0;
+}
