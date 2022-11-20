a05eb3a316c3fbe2e6c176496ff4c8c82a452f50
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 11:58:00 +0530

    Create integers.c

---
+++ integers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1,num2;
+    printf("enter two integers : ");
+    scanf("%d %d",&num1,&num2);
+    printf("sum = %d\n",num1 + num2);
+    //RA2211042010053
+    return 0;
+}
