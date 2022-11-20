49a57cc200da6736406e05fd46259cda419a4ce7
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 16:12:46 +0530

    Create suppression character.c

---
+++ suppression character.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("Enter an integer : ");
+    scanf("%3d%*2d%2d",&a,&b);
+    printf("The values in a and b are : %d,%d\n",a,b);
+    //RA2211042010053
+    return 0;
+}
