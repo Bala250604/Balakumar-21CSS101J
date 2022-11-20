9fa0e9692f3908c8b56d7a4f7375cb149cfd812a
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 15:46:28 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int intValue;
+    float floatValue;
+    printf("Enter an int and a float value : ");
+    scanf("%d %f",&intValue,&floatValue);
+    printf("sum of %d and %f = %f\n",intValue,floatValue,intValue + floatValue);
+    //RA2211042010053
+    return 0;
+}
