c7351e883ab85d4cfff757c9fc8a962517537f3b
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-20 11:53:19 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    char ch[100];
+    printf("Enter a string : ");
+    gets(ch);
+    printf("The entered string is : ");
+    puts(ch);
+    //RA2211042010053
+    return 0;
+}
