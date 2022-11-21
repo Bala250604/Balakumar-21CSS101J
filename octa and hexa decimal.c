5668b4a164b690cf005ebdcb1409e1eb1f1b9df7
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-21 18:29:00 +0530

    Create octa and hexa decimal.c

---
+++ octa and hexa decimal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a decimal integer : ");
+    scanf("%d",&num);
+    printf("Octal number : %o\n",num);
+    printf("Hexadecimal number : %x\n",num);
+    //RA2211042010053
+    return 0;
+}
