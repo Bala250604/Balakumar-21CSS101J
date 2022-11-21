cd979a18abb2aaa2049f1dfc1685cd8b9196044c
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-21 19:14:37 +0530

    Create to check a and b are equal.c

---
+++ to check a and b are equal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 3;
+    float b = 3.0;
+    if (a == b)
+        printf("a and b are equal");
+    if (a != b)
+        printf("a and b are not equal"); 
+    //RA2211042010053
+    return 0;
+}
