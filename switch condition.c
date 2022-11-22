248ac844f19cdffb8de211365a5852f518388011
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-22 09:20:10 +0530

    Create switch condition.c

---
+++ switch condition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int choice = -2 ;
+	switch (choice) {
+		case -1:
+			printf("Case: -1\n");
+			break;
+		case -2:
+			printf("Case: -2\n");
+			break;
+	}
+    //RA2211042010053
+    return 0;
+}
