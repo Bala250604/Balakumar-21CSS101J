e47e0c9b5cf4d8295d2873936bb69216b73bcdb8
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-22 08:58:26 +0530

    Create switch case.c

---
+++ switch case.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int i = 1;
+	switch (i) {
+		printf("This is c program");
+		case 1:
+			printf("Case-1");
+			break;
+		case 2:
+			printf("Case-2");
+			break;
+    //RA2211042010053
+    return 0;
+}
