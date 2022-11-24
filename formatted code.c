14b4cd713ca162f5877cb263c919ded8290ee117
Bala250604 <113957531+Bala250604@users.noreply.github.com>
2022-11-24 23:20:29 +0530

    Create formatted code.c

---
+++ formatted code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a = 20, b = 30;
+	  printf("The sum of two given values = %d\n", a+b);
+	  printf("Hello CodeTantra!");
+    //RA2211042010053
+    return 0;
+}
